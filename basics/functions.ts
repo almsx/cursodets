e8eaba4ff2ebf85e687ded8698601d9a7fec3c39
Alberto Luebbert <>
2023-04-17 07:36:26 -0600

    :sparkles: Se agregan funciones

---
+++ basics/functions.ts
@@ -0,0 +1,11 @@
+const sayHi = (name) => {
+  console.log("Hola", name);
+};
+
+sayHi("Alberto");
+
+function sayGoodBye(name) {
+  console.log(`Adios amigo ${name}`);
+}
+
+sayGoodBye("Beto");
---
+++ dist/functions.js
@@ -0,0 +1,8 @@
+var sayHi = function (name) {
+    console.log("Hola", name);
+};
+sayHi("Alberto");
+function sayGoodBye(name) {
+    console.log("Adios amigo ".concat(name));
+}
+sayGoodBye("Beto");
