f6e000a2fb57367bb357420abecbb9ff2fea6fda
Alberto Luebbert <>
2023-04-16 23:11:23 -0600

    :sparkles: Se agregan interfaces vs classes

---
+++ basics/interfacesvsclasses.ts
@@ -0,0 +1,37 @@
+interface PersonInterface {
+  name: string;
+  code: string | number;
+  charge: number;
+  description?: string;
+  token: string;
+  sayHello: () => string;
+}
+
+class PersonClass {
+  sayHello() {
+    console.log("grrrr");
+    fetch("...");
+  }
+}
+
+//const personOne: PersonInterface = {};
+const personTwo: PersonClass = new PersonClass();
+personTwo.sayHello();
+
+interface PetInterface {
+  sayHello: () => string;
+}
+
+class PetClass {
+  sayHello() {
+    return "Hola";
+  }
+}
+
+class Dog implements PetInterface {
+  sayHello() {
+    return "Hola desde Dog";
+  }
+}
+
+class Cat extends PetClass {}
